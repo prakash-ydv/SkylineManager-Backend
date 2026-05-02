b6b74268c8b7a7da865ccaa3b7d204d234a9044e
Prakash Kumar <prakashyadav9576@gmail.com>
2026-05-02 14:34:35 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // Adjust to frontend URL
+    origin: ['http://localhost:5173', 'https://skyline-manager-seven.vercel.app'],
     credentials: true
 }));
 
