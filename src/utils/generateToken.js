7060bff62c0a15389c9536f38915d6271ab578e8
Prakash Kumar <prakashyadav9576@gmail.com>
2026-05-02 15:38:26 +0530

    auth bug fix

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
+    secure: true, // Required for sameSite: 'none'
+    sameSite: 'none', // Allow cross-site cookies (Vercel to Render)
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
